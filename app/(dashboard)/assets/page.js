8160ea1b3344047cdff82a7d74eb630ed0bac40b
Gautam Raj <ashragautam25@gmail.com>
2024-01-01 18:09:53 +0530

    Bug fix

--- app/(dashboard)/assets/page.js
+++ app/(dashboard)/assets/page.js
@@ -3,11 +3,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-export const metadata = {
-  title: "B-Wallet | Assets",
-  description: "By Bankless DAO",
-};
-
 export default function Assets() {
   const router = useRouter();
 
