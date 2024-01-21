f9b4efd530411ee6340efc4e41bac10ea24ce9d3
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-01-21 21:04:07 +0530

    Added Passkey Integration

--- components/layout/onBoard/setup/SetupHero.js
+++ components/layout/onBoard/setup/SetupHero.js
@@ -1,14 +1,12 @@
-"use client"
+"use client";
 import Image from "next/image";
 
 const SetupHero = () => {
-  return <div>
-    <Image
-      src="/images/setup/setupWallet.png"
-      width={1000}
-      height={1000}
-    />
-  </div>;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <Image src="/images/setup/setupWallet.png" width={500} height={500} />
+    </div>
+  );
 };
 
 export default SetupHero;
--- components/layout/onBoard/setup/step/SetupAccount.js
+++ components/layout/onBoard/setup/step/SetupAccount.js
@@ -1,13 +1,30 @@
+"use client";
 import Image from "next/image";
+import { client } from "@passwordless-id/webauthn";
+
+const challenge = "a7c61ef9-dc23-4806-b486-2428938a547e";
 
 const SetupAccount = () => {
-  return(
-    <div className="flex item-center justify-center">
+  const handleAuth = async () => {
+    const registration = await client.register("Anoy", challenge, {
+      authenticatorType: "auto",
+      userVerification: "discouraged",
+      timeout: 60000,
+      attestation: false,
+      debug: false,
+    });
+
+    console.log(registration);
+  };
+
+  return (
+    <div className="item-center flex justify-center">
       <Image
         src="/images/setup/passKey.png"
         width={440}
         height={440}
         className="hover:cursor-pointer"
+        onClick={handleAuth}
       />
     </div>
   );
